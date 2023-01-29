93847245e3ceba6dcfb4ee720842e4671e9074e9
datawhales <datawhales@gmail.com>
2023-01-30 00:46:15 +0900

    :sparkles: Add random/sampling.py
    Signed-off-by: datawhales <datawhales@gmail.com>

---
+++ data_analysis/random/README.md
@@ -0,0 +1,2 @@
+# Random
+시뮬레이션을 구현할 때 random sampling 등 random 라이브러리를 이용하는 경우에 사용할 수 있는 코드 모음.
---
+++ data_analysis/random/sampling.py
@@ -0,0 +1,49 @@
+from typing import List, Any
+import random
+
+
+def sample_one_int(low: int, high: int) -> int:
+    """두 정수 사이의 임의의 정수 하나를 샘플링하는 함수."""
+    SEED = 42
+    random.seed(SEED)
+
+    # low <= N <= high 를 만족하는 임의의 정수 N을 샘플링
+    ret = random.randint(
+        a=low,
+        b=high,
+    )
+    return ret
+
+
+def sample_one_int_with_step(start: int, stop: int, step: int):
+    """두 정수 중 작은 정수부터 일정한 간격을 가지며 증가하는 수열에서 임의의 정수 하나를 샘플링하는 함수."""
+    SEED = 42
+    random.seed(SEED)
+
+    # start부터 시작하여 일정한 간격 step으로 증가하는 수열에서 임의의 정수 N을 샘플링
+    ret = random.randrange(
+        start=start,
+        stop=stop,
+        step=step,
+    )
+    return ret
+
+
+def shuffle_list(x: List[Any]) -> List:
+    SEED = 42
+    random.seed(SEED)
+
+    # 리스트 x를 셔플
+    random.shuffle(x=x)
+    return x
+
+# https://docs.python.org/ko/3.9/library/random.html
+# random.sample
+# random.choice
+# random.random
+# random.uniform
+# random.gauss
+
+if __name__ == "__main__":
+    x = [1,2,3,4,5,6,7,8,9,10]
+    print(shuffle_list(x))

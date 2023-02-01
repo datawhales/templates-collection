58ac0c4ce3ced895d31b198179c596489e3a13a6
datawhales <datawhales@gmail.com>
2023-02-01 10:40:55 +0900

    :art: Numpy: update array.py
    Signed-off-by: datawhales <datawhales@gmail.com>

--- data_analysis/numpy/array.py
+++ data_analysis/numpy/array.py
@@ -3,15 +3,41 @@ import numpy as np
 
 
 # np.reshape()
-def modify_dim(a: np.ndarray, shape: Tuple[int]) -> np.ndarray:
-    ret = a.reshape(shape)
+def reshape(x: np.ndarray, shape: Tuple[int]) -> np.ndarray:
+    """Numpy array의 dimension은 변화 없이 shape을 변환하는 함수."""
+    ret = x.reshape(shape)
+    return ret
+
+
+# np.squeeze()
+def squeeze(x: np.ndarray, axis: int = 0) -> np.ndarray:
+    """Numpy array의 single-dimension을 제거하는 함수."""
+    ret = x.squeeze(axis=axis)
     return ret
 
 
 def example():
-    a = np.array([1, 2, 3, 4])
-    b = modify_dim(a=a, shape=(1, 4))
-    print(b)
+    a = np.array([[1, 2], [3, 4]])
+    b = reshape(x=a, shape=(1, 4))
+    print(f"b: {b}")
+    c = squeeze(x=b, axis=0)
+    print(f"c: {c}")
+
+
+def example_2():
+    a = np.array([[1, 2], [3, 4]])
+    b = reshape(x=a, shape=(-1, 1))
+    print(f"b: {b}")
+    c = squeeze(x=b, axis=1)
+    print(f"c: {c}")
+
+
+def main():
+    print("Example 1:")
+    example()
+    print("\nExample 2:")
+    example_2()
+
 
 if __name__ == "__main__":
-    example()
+    main()

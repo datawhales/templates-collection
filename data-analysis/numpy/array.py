86d4f187baad7fa90b035ef60b0905badfb63d34
datawhales <datawhales@gmail.com>
2023-02-19 13:25:25 +0900

    :art: Numpy: modified array.py
    Signed-off-by: datawhales <datawhales@gmail.com>

--- data-analysis/numpy/array.py
+++ data-analysis/numpy/array.py
@@ -2,46 +2,74 @@ from typing import Tuple
 import numpy as np
 
 
+# np.ones_like()
+class OnesLikeArray:
+    """Shape은 같고 1로 채워진 np.ndarray."""
+    @staticmethod
+    def ones_like():
+        x = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+        
+        # ones_like
+        ret = np.ones_like(x)
+        print(ret)
+        return ret
+    
+
 # np.reshape()
-def reshape(x: np.ndarray, shape: Tuple[int]) -> np.ndarray:
-    """Numpy array의 dimension은 변화 없이 shape을 변환하는 함수."""
-    ret = x.reshape(shape)
-    return ret
+class ReshapeArray:
+    """Numpy array의 dimension은 변화 없이 shape을 변환."""
+    @staticmethod
+    def reshape():
+        x = np.array([[1, 2], [3, 4]])
+
+        # reshape
+        ret = x.reshape((1, 4))
+        print(ret)
+        return ret
+
+    @staticmethod
+    def reshape_2():
+        x = np.array([[1, 2], [3, 4]])
+
+        # reshape
+        ret = x.reshape((-1, 1))
+        print(ret)
+        return ret
 
 
 # np.squeeze()
-def squeeze(x: np.ndarray, axis: int = 0) -> np.ndarray:
-    """Numpy array의 single-dimension을 제거하는 함수."""
-    ret = x.squeeze(axis=axis)
-    return ret
+class SqueezeArray:
+    """Numpy array의 single-dimension을 제거."""
+    @staticmethod
+    def squeeze():
+        x = np.array([[1, 2, 3, 4]])
 
+        # squeeze
+        ret = x.squeeze(axis=0)
+        print(ret)
+        return ret
 
-def example():
-    a = np.array([[1, 2], [3, 4]])
+    @staticmethod
+    def squeeze_2():
+        x = np.array([[1], [2], [3], [4]])
 
-    # reshape
-    b = a.reshape((1, 4))
-    print(f"b: {b}")
-
-    # reduce dimension
-    c = b.squeeze(axis=0)
-    print(f"c: {c}")
-
-
-def example_2():
-    a = np.array([[1, 2], [3, 4]])
-
-    # reshape
-    b = a.reshape((-1, 1))
-    print(f"b: {b}")
-
-    # reduce dimension
-    c = b.squeeze(axis=1)
-    print(f"c: {c}")
+        # squeeze
+        ret = x.squeeze(axis=1)
+        print(ret)
+        return ret
 
 
 if __name__ == "__main__":
-    print("Example 1:")
-    example()
-    print("\nExample 2:")
-    example_2()
+    # np.ones_like()
+    print(f"np.ones_like()")
+    OnesLikeArray.ones_like()
+
+    # np.reshape()
+    print(f"np.reshape()")
+    ReshapeArray.reshape()
+    ReshapeArray.reshape_2()
+
+    # np.squeeze()
+    print(f"np.squeeze()")
+    SqueezeArray.squeeze()
+    SqueezeArray.squeeze_2()
